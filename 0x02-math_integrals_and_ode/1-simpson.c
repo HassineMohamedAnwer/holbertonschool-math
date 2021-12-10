c9dac4e1c3e59d948522b830b767fd8267c6123b
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-12-10 13:00:12 -0800

    0x02-m

---
+++ 0x02-math_integrals_and_ode/1-simpson.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+/**
+* simpson - check the code
+* @a: first number
+* @b: second number
+* @steps: steps
+* Return: float double
+*/
+
+double simpson(double a, double b, int steps)
+{
+	double r = 0.0, x = (b - a) / steps, h;
+	int i;
+
+	for (i = 0; i < steps - 1; i++)
+	{
+		h = a + i * x;
+		if (i % 2 == 0)
+		{
+			r += 2 / (1 + h * h);
+		}
+		else
+		{
+			r += 4 / (1 + h * h);
+		}
+	}
+r += (1 / (1 + a * a)) + (1 / (1 + b * b));
+r = r * (x / 3);
+
+return (r);
+}
---
+++ 0x02-math_integrals_and_ode/simpson.h
@@ -0,0 +1,8 @@
+#ifndef SMSN_H
+#define SMSN_H
+
+#include <stdio.h>
+#include <stdlib.h>
+
+double simpson(double a, double b, int steps );
+#endif
