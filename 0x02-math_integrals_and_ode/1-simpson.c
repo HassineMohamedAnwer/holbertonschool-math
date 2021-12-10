1b0c12a60d80aca1b4c08c3a3c733832cf19fa7c
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-12-10 13:11:14 -0800

    0x02-m01

--- 0x02-math_integrals_and_ode/1-simpson.c
+++ 0x02-math_integrals_and_ode/1-simpson.c
@@ -14,7 +14,7 @@ double simpson(double a, double b, int steps)
 	double r = 0.0, x = (b - a) / steps, h;
 	int i;
 
-	for (i = 0; i < steps - 1; i++)
+	for (i = 1; i <= steps - 1; i++)
 	{
 		h = a + i * x;
 		if (i % 2 == 0)
